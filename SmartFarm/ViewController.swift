472fc75f12846726f06853d1a093b09ce09e8666
Yani Buchkov <Yani.Buchkov@adastragrp.com>
2019-01-10 15:14:09 +0200

    Initial Commit

---
+++ SmartFarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartFarm
+//
+//  Created by Yani Buchkov on 10.01.19.
+//  Copyright © 2019 Yani Buchkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
